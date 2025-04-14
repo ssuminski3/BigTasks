4f75fb8f0890f42b24093a2e9e4367dfcbe29d40
ssuminski3 <ssuminski3@gmail.com>
2025-04-14 22:10:54 +0200

    Show Big Task

--- client/BigTask/src/pages/ShowBigTask.tsx
+++ client/BigTask/src/pages/ShowBigTask.tsx
@@ -1,9 +1,47 @@
+import { useState } from 'react';
+import "../App.css"
+import TaskComponent from '../components/TaskComponent';
+import { useParams } from 'react-router-dom';
 
-function ShowBigTask() {
+type Task = {
+  name: string;
+  done: boolean;
+  key_my: number;
+};
 
-    return (<>
-        <h1>ShowBigTask</h1>
-    </>)
-}
+const ShowBigTask = () => {
+  const [tasks] = useState<Task[]>([]);
+  const params = useParams();
+  const [text] = useState(params.id);
 
-export default ShowBigTask
+  return (
+    <div className="h-screen back p-4 flex flex-col">
+      {/* Top right text edit section */}
+      <div className="flex justify-start mb-4">
+        <p className="w-1/3 text-2xl">
+            {text}
+        </p>
+      </div>
+
+      {/* Tasks list display */}
+      <div className="flex-grow bg-white rounded shadow p-4 overflow-auto">
+        <h2 className="text-xl font-bold mb-2">Tasks</h2>
+        {tasks.length === 0 ? (
+          <p className="text-gray-500">No tasks added.</p>
+        ) : (
+          <ul>
+            {tasks.map((task) => (
+              <TaskComponent
+                name={task.name}
+                key={task.key_my}
+                key_my={task.key_my}
+                done={false} />
+            ))}
+          </ul>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default ShowBigTask;
