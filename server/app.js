3e3d1a5283f6a58fdcb4f8e93ca5c4155470f0a1
ssuminski3 <ssuminski3@gmail.com>
2025-04-18 22:46:00 +0200

    Deploy 6 Chat GPT says that '/' fault

--- server/app.js
+++ server/app.js
@@ -8,7 +8,7 @@ const sprintRouter = require('./routes/sprints');
 const app = express();
 
 const corsOptions = {
-    origin: 'https://big-tasks-d7jt.vercel.app/',  // Replace with your front-end URL
+    origin: 'https://big-tasks-d7jt.vercel.app',  // Replace with your front-end URL
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true,
 };
