be54f0253cdf7f98d3ec0180c20910f1db56ab23
ssuminski3 <ssuminski3@gmail.com>
2025-04-18 22:36:26 +0200

    Deploy 3

--- server/server.js
+++ server/server.js
@@ -3,6 +3,8 @@ const app = require('./app');
 
 const port = 3000;
 
-const server = app.listen(port, () => {
+app.listen(port, () => {
     console.log(`Server running on http://localhost:${port}/`);
 });
+
+module.exports = app
