be5fc916133cb64681f386c6637cfa05631435a5
ssuminski3 <ssuminski3@gmail.com>
2025-04-18 22:40:05 +0200

    Deploy 4

--- server/app.js
+++ server/app.js
@@ -3,23 +3,24 @@ const cors = require('cors');
 const { jwtCheck } = require('./middleware/auth');
 const bigTasksRoutes = require('./routes/bigTasks');
 const { notFoundHandler, errorHandler } = require('./middleware/errorHandler');
-const taskRouter = require('./routes/tasks')
-const sprintRouter = require('./routes/sprints')
+const taskRouter = require('./routes/tasks');
+const sprintRouter = require('./routes/sprints');
 const app = express();
 
 const corsOptions = {
     origin: 'https://big-tasks-d7jt.vercel.app/',  // Replace with your front-end URL
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],  // Add the methods you need
-    credentials: true,  // If you're using cookies, credentials should be enabled
-  };
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true,
+};
 
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use(jwtCheck);
 app.use(bigTasksRoutes);
-app.use(taskRouter)
-app.use(sprintRouter)
+app.use(taskRouter);
+app.use(sprintRouter);
 app.use(notFoundHandler);
 app.use(errorHandler);
 
-module.exports = app;
+// Instead of app.listen(), use this export for Vercel
+module.exports = (req, res) => app(req, res);
